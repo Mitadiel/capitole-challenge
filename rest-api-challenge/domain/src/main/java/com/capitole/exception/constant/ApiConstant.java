7f07dbf731b03e85810b3b73428ab6b108ed6f8f
Eze <mitadiel94@gmail.com>
2023-12-30 07:07:14 -0300

    feat: Add exceptions

---
+++ rest-api-challenge/domain/src/main/java/com/capitole/exception/constant/ApiConstant.java
@@ -0,0 +1,5 @@
+package com.capitole.exception.constant;
+
+public class ApiConstant {
+    public static final String ID_CAN_NOT_BE_NULL_MESSAGE_ERROR = "Id cant not be null %s";
+}
--- rest-api-challenge/infrastructure/src/main/java/com/capitole/entryPoint/rest/advice/ApiControllerAdvice.java
+++ rest-api-challenge/infrastructure/src/main/java/com/capitole/entryPoint/rest/advice/ApiControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.capitole.entryPoint.rest.advice;
 
 import com.capitole.exception.BusinessException;
+import com.capitole.exception.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,7 +10,18 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ApiControllerAdvice {
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<String> handleEmptyInput(BusinessException emptyInputException){
-        return new ResponseEntity<>(emptyInputException.getErrorMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    public ResponseEntity<String> handleBusinessInput(BusinessException businessException){
+        return new ResponseEntity<>(businessException.getErrorMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
     }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEmptyInput(EntityNotFoundException entityNotFoundException){
+        return new ResponseEntity<>(entityNotFoundException.getErrorMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<String> handleNullInput(NullPointerException nullPointerException){
+        return new ResponseEntity<>(nullPointerException.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
 }
